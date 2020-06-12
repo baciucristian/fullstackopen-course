879786ec896eb6e4b904013c4c5feeb35e1892e3
Cristian Baciu <icristianbaciu@gmail.com>
2020-06-12 19:14:44 +0300

    Initial commit

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Temp</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
