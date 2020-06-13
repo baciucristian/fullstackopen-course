f2baf408fc7730fc350f61f88c9a160666fb9281
Cristian Baciu <icristianbaciu@gmail.com>
2020-06-13 18:24:34 +0300

    Solution 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,13 +5,16 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Total = ({text, value}) => <p>{text + ' ' + value}</p>;
+const Statistic = ({text, value}) => <p>{text + ' ' + value}</p>;
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  let all = good + neutral + bad;
+  let average = (good * 1 + neutral * 0 + bad * -1) / all;
+  let positivePercentage = (good / all) * 100;
 
   return (
     <>
@@ -24,9 +27,12 @@ const App = () => {
       <Button handleClick={() => setBad(bad + 1)} text="bad"></Button>
 
       <h1>Statistics</h1>
-      <Total text="good" value={good}></Total>
-      <Total text="neutral" value={neutral}></Total>
-      <Total text="bad" value={bad}></Total>
+      <Statistic text="good" value={good}></Statistic>
+      <Statistic text="neutral" value={neutral}></Statistic>
+      <Statistic text="bad" value={bad}></Statistic>
+      <Statistic text="all" value={all}></Statistic>
+      <Statistic text="average" value={average}></Statistic>
+      <Statistic text="positive" value={positivePercentage + ' %'}></Statistic>
     </>
   );
 };
