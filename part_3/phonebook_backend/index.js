f13c0c337c1715de7c8e83902296ef09768b2692
Cristian Baciu <icristianbaciu@gmail.com>
2020-07-13 21:57:22 +0300

    Solution 3.17

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -66,6 +66,22 @@ app.post('/api/persons', (req, res) => {
   });
 });
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+      console.log(updatedPerson);
+      res.json(updatedPerson);
+    })
+    .catch(error => next(error));
+});
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({error: 'unknown endpoint'});
 };
