05f29be13abe168fa9dccf250afb6b37e8c06d1a
Cristian Baciu <icristianbaciu@gmail.com>
2020-08-05 17:27:59 +0300

    Solution 4.10

--- part_4/bloglist/backend/tests/bloglist_api.test.js
+++ part_4/bloglist/backend/tests/bloglist_api.test.js
@@ -30,6 +30,28 @@ test('unique identifier property of the blog is named id,', async () => {
   expect(blogsInDb[0].id).toBeDefined();
 });
 
+test('a valid blog can be added,', async () => {
+  const newBlog = {
+    title: 'The future of spaceflight',
+    author: 'Nadia Drake',
+    url:
+      'https://www.nationalgeographic.com/science/space/space-exploration/future-spaceflight/',
+    likes: 145,
+  };
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const contents = blogsAtEnd.map(blog => blog.title);
+  expect(contents).toContain('The future of spaceflight');
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
