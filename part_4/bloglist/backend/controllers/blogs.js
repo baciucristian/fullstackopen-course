a7c1684c6f00ad0cd28eda51b019bfe9edd991d7
Cristian Baciu <icristianbaciu@gmail.com>
2020-08-23 17:33:51 +0300

    Solution 4.20

--- part_4/bloglist/backend/controllers/blogs.js
+++ part_4/bloglist/backend/controllers/blogs.js
@@ -3,14 +3,6 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-const getTokenFrom = req => {
-  const authorization = req.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7);
-  }
-  return null;
-};
-
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user', {username: 1, name: 1});
   res.json(blogs);
@@ -24,9 +16,8 @@ blogsRouter.get('/:id', async (req, res) => {
 blogsRouter.post('/', async (req, res) => {
   const body = req.body;
 
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!token || !decodedToken.id) {
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+  if (!req.token || !decodedToken.id) {
     return res.status(401).json({error: 'token missing or invalid'});
   }
   const user = await User.findById(decodedToken.id);
--- part_4/bloglist/backend/utils/middleware.js
+++ part_4/bloglist/backend/utils/middleware.js
@@ -8,6 +8,14 @@ const requestLogger = (req, res, next) => {
   next();
 };
 
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    req.token = authorization.substring(7);
+  }
+  next();
+};
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({error: 'unknown endpoint'});
 };
@@ -34,4 +42,5 @@ module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
 };
