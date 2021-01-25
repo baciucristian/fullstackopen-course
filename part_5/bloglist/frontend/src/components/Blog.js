c923db407d8db5e5a284d13d70a2b98e403956bb
Cristian Baciu <icristianbaciu@gmail.com>
2021-01-25 21:09:41 +0200

    Solution 5.7

--- part_5/bloglist/frontend/src/components/Blog.js
+++ part_5/bloglist/frontend/src/components/Blog.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenVisible = { display: visible ? '' : 'none' };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        {`${blog.title} ${blog.author}`}{' '}
+        <button type="button" onClick={toggleVisibility}>
+          view
+        </button>
+      </p>
+      <div style={showWhenVisible}>
+        {blog.url} <br />
+        likes {blog.likes} <button type="button">like</button> <br />
+        {blog.user ? blog.user.name : null} <br />
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
