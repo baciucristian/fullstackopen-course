aaeccb86589bc54aad048f96590a57bc0d94fb5e
baciucristian <icristianbaciu@gmail.com>
2023-08-24 13:06:20 +0300

    Update cloned repository

--- part_6/unicafe-redux/src/reducer.js
+++ part_6/unicafe-redux/src/reducer.js
@@ -1,23 +1,23 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0,
-};
+  bad: 0
+}
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action);
+  console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return { ...state, good: state.good + 1 };
+      return state
     case 'OK':
-      return { ...state, ok: state.ok + 1 };
+      return state
     case 'BAD':
-      return { ...state, bad: state.bad + 1 };
+      return state
     case 'ZERO':
-      return { good: 0, ok: 0, bad: 0 };
-    default:
-      return state;
+      return state
+    default: return state
   }
-};
+  
+}
 
-export default counterReducer;
+export default counterReducer
