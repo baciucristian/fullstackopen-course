ef2be9524bb5cc454e358ef84dcfda281b66a032
baciucristian <icristianbaciu@gmail.com>
2023-09-04 17:50:00 +0300

    Solution 6.9

--- part_6/redux-anecdotes/src/components/AnecdoteList.js
+++ part_6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,7 +2,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteForm = () => {
-	const anecdotes = useSelector(state => state)
+	const anecdotes = useSelector(state => {
+    if ( state.filter === 'ALL' || state.filter === '') {
+      return state.anecdotes
+    }
+    return state.anecdotes.filter(anecdote => {
+			const content = anecdote.content.toLowerCase();
+			return content.includes(state.filter.toLowerCase())
+		})
+  })
+
 	const dispatch = useDispatch();
 
   const vote = (id) => {
