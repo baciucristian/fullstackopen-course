ff83e2f07b3338baea8bc37d007d42f71a9dcc13
baciucristian <icristianbaciu@gmail.com>
2024-05-18 19:11:46 +0300

    Solution 9.4

---
+++ part_9/part-b-exercises/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+app.get('/hello', (_req, res) => {
+	res.send('Hello Full Stack!');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
