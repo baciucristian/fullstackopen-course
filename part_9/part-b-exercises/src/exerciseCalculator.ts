235f23212270427ca18f850845f8be3d6a12d742
baciucristian <icristianbaciu@gmail.com>
2024-11-02 20:57:38 +0200

    Solution 9.7

--- part_9/part-b-exercises/src/exerciseCalculator.ts
+++ part_9/part-b-exercises/src/exerciseCalculator.ts
@@ -99,3 +99,5 @@ try {
 	}
 	console.log(errorMessage);
 }
+
+export { ExercisesParams, ExercisesResult, calculateExercises };
--- part_9/part-b-exercises/src/index.ts
+++ part_9/part-b-exercises/src/index.ts
@@ -1,15 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { isNotNumber } from './utils/isNotNumber';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 import type { BmiValues } from './bmiCalculator';
+import type { ExercisesResult } from './exerciseCalculator';
 
 const app = express();
 const port = 3003;
+
+app.use(express.json());
 
 type ResponseBMI = {
 	height: number;
 	weight: number;
 	BMI: BmiValues;
 };
+
+interface ExerciseRequestBody {
+	daily_exercises: number[];
+	target: number;
+}
 
 app.get('/hello', (_req, res) => {
 	res.send('Hello Full Stack!');
@@ -37,6 +47,27 @@ app.get('/bmi', (req, res) => {
 		console.log(error);
 		res.status(400).send(JSON.stringify({ error: 'malformatted parameters' }));
 	}
+});
+
+app.post('/exercises', (req: Request<never, never, ExerciseRequestBody>, res: Response) => {
+	const { daily_exercises, target } = req.body;
+
+	if (!daily_exercises || !target) {
+		res.status(400).json({ error: 'malformatted parameters' });
+		return;
+	}
+
+	const areDailyExercisesNumbers: boolean = daily_exercises.every((el) => !isNotNumber(el));
+	const isTargetNumber: boolean = !isNotNumber(target);
+
+	if (!areDailyExercisesNumbers || !isTargetNumber) {
+		res.status(400).json({ error: 'malformatted parameters' });
+		return;
+	}
+
+	const exercisesResult: ExercisesResult = calculateExercises(daily_exercises, target);
+
+	res.status(201).json(exercisesResult);
 });
 
 app.listen(port, () => {
